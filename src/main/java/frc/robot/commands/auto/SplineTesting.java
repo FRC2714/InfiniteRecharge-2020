b31d20bb8bf9ea186c4366e95ada742c1d222538
Gagan Bhat <gaganbhat6@gmail.com>
2020-01-15 10:35:27 -0600

    Added instantCommand to reset drivetrain at beginning of autonomous.

--- src/main/java/frc/robot/commands/auto/SplineTesting.java
+++ src/main/java/frc/robot/commands/auto/SplineTesting.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.drivetrain.trajectories.FollowTrajectory;
 import frc.robot.subsystems.Drivetrain;
@@ -16,6 +17,8 @@ public class SplineTesting extends SequentialCommandGroup {
     public SplineTesting(Drivetrain drivetrain){
 
         addCommands(
+                new InstantCommand(drivetrain::resetAll),
+
                 new FollowTrajectory(
                         drivetrain,
                         new Pose2d(Units.feetToMeters(0), Units.feetToMeters(0), new Rotation2d().fromDegrees(0)),
@@ -29,14 +32,12 @@ public class SplineTesting extends SequentialCommandGroup {
                 new FollowTrajectory(
                         drivetrain,
                         new Pose2d(Units.feetToMeters(10), Units.feetToMeters(-4.5), new Rotation2d().fromDegrees(0)),
-                        // Pass through these two interior waypoints, making an 's' curve path
                         List.of(
                                 new Translation2d(Units.feetToMeters(5), Units.feetToMeters(-2.5))
                         ),
                         new Pose2d(Units.feetToMeters(0), Units.feetToMeters(0), new Rotation2d().fromDegrees(0)),
                         4,2,true
                 )
-
         );
 
     }
